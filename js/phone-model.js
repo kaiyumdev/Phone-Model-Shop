2a1b5783a9755574dd2f2626e71dfdf20c7289f5
MdKaioumislam5 <afrankaiyum34@gmail.com>
2022-06-10 16:21:24 +0600

    phone design updated

--- js/phone-model.js
+++ js/phone-model.js
@@ -1,42 +1,80 @@
-const searchFood = () => {
+// searchField
+const searchPhone = () => {
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
-  console.log(searchText);
+  // console.log(searchText);
   searchField.value = "";
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  // console.log(url);
   fetch(url)
     .then((res) => res.json())
     .then((data) => displaySearch(data.data.slice(0, 20)));
-//   console.log(url);
+  //   console.log(url);
 };
+// search show image
 const displaySearch = (phones) => {
   const searchResult = document.getElementById("search-result");
-  searchResult.textContent = '';
+  searchResult.textContent = "";
   phones.forEach((phone) => {
-    console.log(phone);
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `<div class="col">
-    <div onclick="loadPhoneDetail(${phone.brand})" class="card h-100 w-75 mx-auto">
-  <img height="500" width="200" src="${phone.image}" class="card-img-top" alt="...">
+    <div class="card h-100 w-75 mx-auto">
+  <img  src="${phone.image}" class="card-img-top main-img" alt="...">
   <div class="card-body">
-    <h5 class="card-title">${phone.phone_name}</h5>
-    <p class="card-text">${phone.slug}</p>
+    <h5 class="card-title">${phone.brand}</h5>
+    <p class="card-text">${phone.phone_name}</p>
+    <button onclick="loadPhoneDetail('${phone.slug}')" class="card mx-auto btn btn-outline-warning">Details</button>
   </div>
 </div>
 </div>
-        
         `;
     searchResult.appendChild(div);
   });
 };
 
+// show phone image
+const loadPhoneDetail = (phoneId) => {
+  console.log(phoneId);
+  const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+  console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayPhoneDetails(data.data));
+};
 
-const loadPhoneDetail = phoneId => {
-    console.log(phoneId);
-    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
-    console.log(url);
-    fetch(url)
-    .then(res => res.json())
-    .then( data =>console.log(data.data))
-}
+// show phone details
+const displayPhoneDetails = (phone) => {
+  console.log(phone);
+  const phoneDetails = document.getElementById("phone-details");
+  phoneDetails.textContent = "";
+  console.log(phoneDetails);
+  const div = document.createElement("div");
+  div.classList.add("card");
+  div.innerHTML = `
+  <div class="card-details mx-auto">
+  <img class="image-details" src="${phone.image}">
+  <div class="card-body">
+  <h5 class="card-title">Release Date: ${phone.releaseDate}</h5>
+  <p class="card-text">ChepSet: ${phone.mainFeatures.chipSet}</p>
+  <p class="card-text">Display: ${phone.mainFeatures.displaySize}</p>
+  <p class="card-text">Memory: ${phone.mainFeatures.memory}</p>
+  <p class="card-text">Storage: ${phone.mainFeatures.storage}</p>
+  <p class="card-text">Sensors: ${phone.mainFeatures.sensors[0]}</p>
+  <p class="card-text">Sensors: ${phone.mainFeatures.sensors[1]}</p>
+  <p class="card-text">Sensors: ${phone.mainFeatures.sensors[2]}</p>
+  <p class="card-text">Sensors: ${phone.mainFeatures.sensors[3]}</p>
+  <p class="card-text">Sensors: ${phone.mainFeatures.sensors[4]}</p>
+  <p class="card-text">Wlan: ${phone.others.WLAN}</p>
+  <p class="card-text"> Bluetooth: ${phone.others.Bluetooth}</p>
+  <p class="card-text">GPS: ${phone.others.GPS}</p>
+  <p class="card-text">NFC: ${phone.others.NFC}</p>
+  <p class="card-text">Radio: ${phone.others.Radio}</p>
+  <p class="card-text">USB: ${phone.others.USB}</p>
+  </div>
+  </div>
+  
+  `;
+  phoneDetails.appendChild(div);
+};
+
